481fe59896ca82cd59415e369fa6e4f2efe58912
Marcel <kriollik@gmail.com>
2023-10-15 13:03:50 +0200

    feat: add remove button in cart page

--- client/src/pages/cart/index.tsx
+++ client/src/pages/cart/index.tsx
@@ -1,14 +1,25 @@
 import { FC } from "react";
 import { useCartStore } from "../../store/cart";
+import Button from "../../components/Button";
 
 const CartPage: FC = () => {
-  const { items } = useCartStore();
+  const { items, remove } = useCartStore();
+
+  const handleRemoveFromCart = (id: number, amount: number) => {
+    remove({ id, amount });
+  };
 
   return (
     <div>
       {items.map((item) => (
-        <div>
+        <div key={item.id}>
           ID: {item.id}, ammount: {item.amount}
+          <div>
+            <Button
+              label="Remove"
+              onClick={() => handleRemoveFromCart(item.id, 1)}
+            />
+          </div>
         </div>
       ))}
     </div>
