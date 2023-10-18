7ac023e0cd1cf1aa26af67246932e9eca843f010
Marcel <kriollik@gmail.com>
2023-10-18 21:45:56 +0200

    feat: add simple style to cart page

--- client/src/pages/cart/index.tsx
+++ client/src/pages/cart/index.tsx
@@ -10,18 +10,23 @@ const CartPage: FC = () => {
   };
 
   return (
-    <div>
-      {items.map((item) => (
-        <div key={item.id}>
-          ID: {item.id}, ammount: {item.amount}
-          <div>
-            <Button
-              label="Remove"
-              onClick={() => handleRemoveFromCart(item.id, 1)}
-            />
+    <div className="flex flex-row flex-wrap justify-center">
+      <div className="box-border w-4/5 mt-4 bg-white rounded-md shadow-sm">
+        {items.map((item) => (
+          <div key={item.id} className="flex">
+            <div className="table-cell basis-4/5 text-left border-l border-b">
+              ID: {item.id}, ammount: {item.amount}
+            </div>
+            <div className="table-cell basis-1/5 text-right border-r border-b">
+              <Button
+                label="Remove"
+                className=" table-button bg-red-400 hover:bg-red-600"
+                onClick={() => handleRemoveFromCart(item.id, 1)}
+              />
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
